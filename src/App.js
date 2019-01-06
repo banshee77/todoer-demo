ad5bfda298a97078792f256f09444989a7efad30
Wieslaw Pedzich <wieslaw.pedzich@gmail.com>
2019-01-06 15:31:57 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,64 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state= {
+      items: [
+        { name: "Feed the cat", status: "NEW" },
+        { name: "Discover purpose of life", status: "NEW" },
+        { name: "Learn to cloud", status: "NEW" }
+      ]
+    }
+
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main>
+
+        <h1> TODO list </h1>
+        <TodoList items={this.state.items} />
+
+        </main>
+
       </div>
     );
+  }
+}
+
+class TodoList extends Component {
+  render() {
+
+    return (
+      <div className="TodoList">
+        <ul>
+          {
+            this.props.items.map( ( itm, i) => {
+              return (
+                <TodoItem item={itm} key ={i} />
+              )
+            })
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+class TodoItem extends Component {
+  render() {
+    const item = this.props.item;
+
+    return (
+      <li>
+        <input type="checkbox" />
+        {item.name}
+      </li>
+    )
   }
 }
 
